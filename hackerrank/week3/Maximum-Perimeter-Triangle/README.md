2ba625a2ae20726736c7f39a8fa1ea89198cb61e
lilble <77142800+lilble@users.noreply.github.com>
2023-05-05 18:06:15 +0900

    week3-last

---
+++ hackerrank/week3/Maximum-Perimeter-Triangle/README.md
@@ -0,0 +1,7 @@
+Function Description
+
+Complete the maximumPerimeterTriangle function in the editor below.
+
+maximumPerimeterTriangle has the following parameter(s):
+
+int sticks[n]: the lengths of sticks available
---
+++ hackerrank/week3/Sales-by-Match/1-my.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+def sockMerchant(n, ar):
+    # Write your code here
+    d = dict()
+    for color in ar:
+        d[color] = d.get(color, 0) + 1
+    pairs = 0
+    for color in d:
+        pairs += d[color]//2
+    return pairs
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
