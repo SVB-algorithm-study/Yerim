49d8ffd723f97b37c7d6c5b088d78423975b5cf5
lilble <77142800+lilble@users.noreply.github.com>
2023-05-08 15:34:59 +0900

    week4-1

---
+++ hackerrank/week4/Picking-Numbers/1-my.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'pickingNumbers' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def pickingNumbers(a):
+    # Write your code here
+    count = [0]*100
+    for e in a:
+        count[e]+=1
+    ans = 0
+    for i in range(1, 99):
+        ans = max(ans, count[i+1]+count[i])
+    return ans
+        
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = pickingNumbers(a)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
